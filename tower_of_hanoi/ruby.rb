7214285b0c733585e06a4e007056e03193041aeb
Misaki Yanai <yanai@sansan.com>
2017-09-19 19:35:40 +0900

    Solve Tower of Hanoi by ruby

---
+++ tower_of_hanoi/ruby.rb
@@ -0,0 +1,36 @@
+class Hanoi
+  attr_reader :steps
+
+  POLES = [
+    POLE_LEFT   = 'LEFT',
+    POLE_CENTER = 'CENTER',
+    POLE_RIGHT  = 'RIGHT',
+  ]
+
+  def initialize disks
+    @disks = disks
+  end
+
+  def solve
+    return if @disks.zero?
+    hanoi(@disks, POLE_LEFT, POLE_CENTER, POLE_RIGHT)
+  end
+
+  private
+
+  def hanoi disks, pole_a, pole_b, pole_c
+    return if disks.zero?
+
+    hanoi(disks - 1, pole_a, pole_c, pole_b)
+    puts "#{pole_a} => #{pole_b}"
+    hanoi(disks - 1, pole_c, pole_b, pole_a)
+  end
+end
+
+
+puts 'Please enter the number of disks'
+disks = gets.to_i
+puts "Let's solve Tower of Hanoi"
+
+Hanoi.new(disks).solve
+
