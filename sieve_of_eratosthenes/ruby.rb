320271632f778278fc63f815db5fd9580e6c6f92
Misaki Yanai <yanai@sansan.com>
2017-10-20 12:18:06 +0900

    Change loop process to recursive process

--- sieve_of_eratosthenes/ruby.rb
+++ sieve_of_eratosthenes/ruby.rb
@@ -1,21 +1,29 @@
-class PrimeNumber
+class Eratosthene
   BASE_NUMBER = 2
 
   def initialize max_number
-    @max_number = Math.sqrt(max_number).to_i
-    @candidates = (BASE_NUMBER..max_number).to_a
+    @square_root  = Math.sqrt(max_number).to_i
+    @search_list  = (BASE_NUMBER..max_number).to_a
   end
 
-  def search
-    BASE_NUMBER.upto(@max_number).each do |num|
-      @candidates -= @candidates[1..-1].select { |candidate| candidate % num == 0 }
-    end
-    @candidates
+  def sieve
+    search(@search_list, [])
+  end
+
+  private
+
+  def search search_list, prime_list
+    first_number = search_list.first
+    prime_list << first_number
+    search_list -= search_list.select { |number| number % first_number == 0 }
+    return prime_list.concat(search_list) if first_number > @square_root
+
+    search(search_list, prime_list)
   end
 end
 
 puts 'Please enter the number'
 number = gets.to_i
-puts "Let's find Prime Numbers"
+puts "Let's find Prime Numbers using Sieve of Eratosthene"
 
-puts "Prime Number: #{PrimeNumber.new(number).search.join(', ')}"
+puts "Prime Number: #{Eratosthene.new(number).sieve}"
