c49ba44f6770ed4422bb1b2998a4d9bb173c456e
Misaki Yanai <yanai@sansan.com>
2017-10-20 12:31:01 +0900

    Update .py to language version 3

--- tower_of_hanoi/python.py
+++ tower_of_hanoi/python.py
@@ -2,23 +2,22 @@ class Hanoi:
   def __init__(self, disks):
     self.disks = disks
 
-    self.__poleLeft = 'LEFT'
-    self.__poleCenter = 'CENTER'
-    self.__poleRight = 'RIGHT'
+    self.__pole_left = 'LEFT'
+    self.__pole_center = 'CENTER'
+    self.__pole_right = 'RIGHT'
 
   def solve(self):
     if (self.disks):
-      self.hanoi(self.disks, self.__poleLeft, self.__poleCenter, self.__poleRight)
-
+      self.hanoi(self.disks, self.__pole_left, self.__pole_center, self.__pole_right)
   def hanoi(self, disks, pole_a, pole_b, pole_c):
     if (disks):
       self.hanoi(disks - 1, pole_a, pole_c, pole_b)
-      print 'Move: {0} => {1}'.format(pole_a, pole_b)
+      print('Move: {0} => {1}'.format(pole_a, pole_b))
       self.hanoi(disks - 1, pole_c, pole_b, pole_a)
 
-print 'Please enter the number of disks'
-disks = raw_input('>> ')
-print "Let's solve Tower of Hanoi"
+print('Please enter the number of disks')
+disks = input('>> ')
+print("Let's solve Tower of Hanoi")
 
 h = Hanoi(int(disks, 10))
 h.solve()
